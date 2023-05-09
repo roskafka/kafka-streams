1596c0c3ffc51bebdd60fe2772a5654edda1dc23
sobottju <julian.benedikt.sobott@hs-furtwangen.de>
2023-05-09 17:07:07 +0200

    positions algorithm

--- src/main/java/de/hfu/kafkaprocessors/PositionProcessor.java
+++ src/main/java/de/hfu/kafkaprocessors/PositionProcessor.java
@@ -1,6 +1,7 @@
 package de.hfu.kafkaprocessors;
 
 import de.hfu.kafkaprocessors.messages.Message;
+import de.hfu.kafkaprocessors.messages.PayloadPosition;
 import de.hfu.kafkaprocessors.serialization.JSONSerde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
@@ -13,6 +14,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class PositionProcessor {
 
@@ -21,10 +25,49 @@ public class PositionProcessor {
 
         private static final Logger logger = LoggerFactory.getLogger(PositionProcessor.class);
 
+        private static final double THRESHOLD_DISTANCE = 0.1;
+
+        Map<String, PayloadPosition> latestPositions = new HashMap<>();
+
         @Autowired
         public void createPositionsStream(final StreamsBuilder builder) {
+            // KStream<String, Message> positions = builder.stream(INPUT_TOPIC, Consumed.with(Serdes.String(),new JSONSerde<>()));
             KStream<String, Message> positions = builder.stream(INPUT_TOPIC, Consumed.with(Serdes.String(),new JSONSerde<>()));
+            // on every new position check if the 2 robots are close to each other
+            // if so, stop one robot
+            // one position contains the position of one robot
+            // when the robots are far away from each other, start the stopped robot
             positions
-                    .foreach((key, value) -> logger.info("Received message: " + value + " with key: " + key));
+                    .map((key, message) -> new KeyValue<>(message.meta().robot(), message.payload()))
+                    .map((robot, payload) -> {
+                        latestPositions.put(robot, payload);
+                        logger.info("Received position of robot {} at ({}, {})", robot, payload.x(), payload.y());
+                        PayloadPosition otherRobot = getOtherRobot(robot);
+                        boolean stop = false;
+                        if (otherRobot != null) {
+                            stop = stopIfClose(payload.x(), payload.y(), otherRobot.x(), otherRobot.y());
+                        }
+                        logger.info("Stop: {}", stop);
+                        return new KeyValue<>(robot, stop);
+                    }).filter((robot, stop) -> stop)
+                    .mapValues(stop -> "stop")
+                    .to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.String()));
+        }
+
+        private float getDistance(float x1, float y1, float x2, float y2) {
+            return (float) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        }
+
+        private boolean stopIfClose(float x1, float y1, float x2, float y2) {
+            return getDistance(x1, y1, x2, y2) < THRESHOLD_DISTANCE;
+        }
+
+        private PayloadPosition getOtherRobot(String thisRobot) {
+            for (String robot : latestPositions.keySet()) {
+                if (!robot.equals(thisRobot)) {
+                    return latestPositions.get(robot);
+                }
+            }
+            return null;
         }
 }
--- src/main/python/producer.py
+++ src/main/python/producer.py
@@ -1,4 +1,6 @@
 import json
+import time
+from datetime import datetime
 
 from kafka import KafkaProducer
 
@@ -6,11 +8,32 @@ from models import Message, PayloadPosition, MetaData
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: v.json().encode('utf-8'))
 
-for i in range(10):
-    data = Message(
-        payload=PayloadPosition(x=i, y=i, theta=i, linear_velocity=i, angular_velocity=i),
-        meta=MetaData(robot=f"robot-{i % 2}", topic="positions", type="message")
-    )
-    producer.send('positions', key=f"robot-{i % 2}".encode("utf-8"), value=data)
+data = Message(
+    payload=PayloadPosition(x=10, y=0, theta=0, linear_velocity=1, angular_velocity=1),
+    meta=MetaData(robot=f"robot-1", topic="positions", type="message")
+)
+producer.send('positions', key=f"robot-1".encode("utf-8"), value=data)
+
+data = Message(
+    payload=PayloadPosition(x=0, y=0, theta=0, linear_velocity=1, angular_velocity=1),
+    meta=MetaData(robot=f"robot-2", topic="positions", type="message")
+)
+producer.send('positions', key=f"robot-2".encode("utf-8"), value=data)
+
+time.sleep(2)
+
+data = Message(
+    payload=PayloadPosition(x=4.555, y=0, theta=0, linear_velocity=1, angular_velocity=1),
+    meta=MetaData(robot=f"robot-1", topic="positions", type="message")
+)
+producer.send('positions', key=f"robot-1".encode("utf-8"), value=data)
+
+data = Message(
+    payload=PayloadPosition(x=4.5, y=0, theta=0, linear_velocity=1, angular_velocity=1),
+    meta=MetaData(robot=f"robot-2", topic="positions", type="message")
+)
+print("Sending message ts: ", datetime.now())
+producer.send('positions', key=f"robot-2".encode("utf-8"), value=data)
+
 
 producer.flush()
